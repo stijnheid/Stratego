88f6c9575ae3983b027ca35a540e9ef84cd8d065
s146928 <s146928@s146928.campus.tue.nl>
2016-10-20 17:10:34 +0200

    Created the Main.java wrapper class Created the NeuralNetwork.java class handling defender setup and the 		gameboard creation Adjusted the SetupGUI so it adjusts the attacker setup in Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,20 +5,47 @@
  */
 package Main;
 
+import Game.GameBoard;
+import NeuralNetwork.NeuralNetwork;
+import Renderer.SetupGUI;
+import java.io.IOException;
+
 /**
  * Main class which handles creating all the objects and running the game
  * @author s146928
  */
-public class Main {
+public  class Main {
+    
+    int[] attackersetup;
+    GameBoard board;
+    
+    public Main() throws IOException{
+        SetupGUI setupGUI = new SetupGUI(this); // creates a SetupGUI object, creating an attacker setup and storing this in the instance variable
+        
+        //The neural net requires the CORRECT attacker setup, so in between here there needs to be a callback to make sure we don't run this code before the user is
+        // done creating a setup 
+        NeuralNetwork neuralnetwork = new NeuralNetwork(this, attackersetup); // creates a NeuralNetwork object, creating a defensive setup and a complete GameBoard, and storing this in the instance variable
+        
+        // Here we have the GameBoard object, use this to create the game itself
+        // Something needs to call the animations too!
+        
+    }
     
     public static void main(String[] args){
-        try{
-            
-            
-            
-        }catch (Exception e){
-            // Put something here when an exception is caught. 
-        }
+         try{    
+            Main main = new Main();
+         } catch (Exception e){
+             
+         }
+   
+    }
+    
+    public void setAttackerSetup(int[] setup){
+        this.attackersetup = setup;
+    }
+    
+    public void setGameBoard(GameBoard board){
+        this.board = board;
     }
     
     
--- src/Renderer/AnimationCallback.java
+++ src/Renderer/AnimationCallback.java
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,4 +12,3 @@ public interface AnimationCallback {
     
     public void animationEnded();
 }
-
