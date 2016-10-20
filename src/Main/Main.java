16d8d2e9d4804f7ec1bcfaa177ce80770222feb8
s146928 <s146928@s146928.campus.tue.nl>
2016-10-20 14:26:11 +0200

    Creating the wrapper class

---
+++ src/Main/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ * Main class which handles creating all the objects and running the game
+ * @author s146928
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        try{
+            
+            
+            
+        }catch (Exception e){
+            // Put something here when an exception is caught. 
+        }
+    }
+    
+    
+}
